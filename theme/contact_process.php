b6b5953b777b068d97f8969abd4c5920d0be7feb
Ozioma Ozor-Ilo <71114453+ozi13@users.noreply.github.com>
2023-12-15 22:54:09 -0500

    initial files commit

---
+++ theme/contact_process.php
@@ -0,0 +1,38 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    // Set the recipient email address.
+    $recipient = "gfli-officers@mit.edu"; // replace with your email address
+
+    // Set the email subject.
+    $subject = "New contact from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Subject: $subject\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
